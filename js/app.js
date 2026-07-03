af6c147484fc4fb23fa2304d48c7e7de25851a03
mxgorbigmx <mepg790619@gmail.com>
2026-07-03 10:02:38 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,20 +1,34 @@
 document.addEventListener("DOMContentLoaded",()=>{
-
     console.log("Proyecto Aurora iniciado ✨");
-
 });
 
 const seal=document.querySelector(".seal");
-
 const flap=document.querySelector(".envelope-flap");
-
+const flap = document.querySelector(".flap");
 const letter=document.querySelector(".letter");
 
-seal.addEventListener("click",()=>{
+seal.addEventListener("click", () => {
 
-    flap.style.transform="rotateX(180deg)";
+    const tl = gsap.timeline();
+    tl.to(".seal",{
+        scale:.85,
+        duration:.15
+    })
 
-    letter.style.transform="translateY(-140px)";
+    .to(".seal",{
+        scale:1,
+        duration:.15
+    })
 
+    .to(".flap",{
+        rotateX:-180,
+        duration:1.2,
+        ease:"power2.inOut"
+    })
+
+    .to(".letter",{
+        y:-150,
+        duration:1.2,
+        ease:"power4.out"
+    },"-=0.8");
 });
-

36572e3f312fa5be6fd621939464c22fd8f5047d
mxgorbigmx <mepg790619@gmail.com>
2026-07-04 13:20:16 -0600

    Add files via upload

--- js/envelope.js
+++ js/envelope.js
@@ -1,55 +1,15 @@
 async function initEnvelope(){
+
     const response = await fetch("assets/svg/production/envelope.svg");
+
     const svg = await response.text();
-    document
-        .querySelector(".envelope-container")
-        .innerHTML = svg;
-    initEnvelopeAnimation();
-}
 
-function initEnvelopeAnimation(){
-    gsap.set("#letter",{
-        y:0
-    });
-}
+    document.querySelector(".envelope-container").innerHTML = svg;
 
-function initEnvelope(){
-    gsap.set("#hero",{
-        opacity:0
-    });
-    const seal=document.querySelector(".seal");
-    const flap=document.querySelector(".flap");
-    const letter=document.querySelector(".letter");
-    seal.addEventListener("click",()=>{
-        const tl=gsap.timeline();
-        tl.to(".seal",{
-            scale:.85,
-            duration:.15
-        })
-        .to(".seal",{
-            scale:1,
-            duration:.15
-        })
-        .to(".flap",{
-            rotateX:-180,
-            duration:1.2,
-            ease:"power2.inOut"
-        })
-        .to(".letter",{
-            y:-150,
-            duration:1.2,
-            ease:"power4.out"
-        },"-=0.8")
-        .to("#hero",{
-            opacity:1,
-            duration:1.5
-        },"-=0.5");
-    });
-}
+    console.log("SVG cargado");
 
-gsap.to("#flap", {
-    rotationX: -180,
-    transformOrigin: "50% 0%",
-    duration: 1.2,
-    ease: "power2.inOut"
-});
+    console.log(document.querySelector("#flap"));
+    console.log(document.querySelector("#letter"));
+    console.log(document.querySelector("#seal"));
+
+}
